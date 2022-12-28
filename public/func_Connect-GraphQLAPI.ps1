7fd783bff0cf846032d08fda7b4459aeacbc2954
Jake Robinson <jake.robinson@rubrik.com>
2022-12-28 13:50:26 -0500

    Connect-GraphQLAPI.ps1

---
+++ public/func_Connect-GraphQLAPI.ps1
@@ -0,0 +1,33 @@
+function Connect-GraphQLAPI {
+    [CmdletBinding()]
+    param (
+        # Name of the dynamic module created. This will also be the noun prefix given to generated cmdlets.
+        [Parameter]
+        [String]
+        $Name,
+        # URI of the GraphQL API. e.g. https://example.com/api/graphql
+        [Parameter]
+        [string]
+        $Uri,
+        # Hashtable of HTTP headers required by the GraphQL API. 
+        [Parameter]
+        [hashtable]
+        $Headers,
+        # If introspection is disabled on the API, you will need to provide the API schema in JSON format.
+        [Parameter]
+        [string]
+        $SchemaPath
+    )
+    
+    begin {
+        
+    }
+    
+    process {
+        
+    }
+    
+    end {
+        
+    }
+}
