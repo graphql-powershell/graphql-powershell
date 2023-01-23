c1cc9a3acf1c5f99e574abb16dbacb1b6177935c
Mike Preston <mwpreston@gmail.com>
2023-01-23 13:08:18 -0500

    removed blankspace

--- public/func_Connect-GraphQLAPI.ps1
+++ public/func_Connect-GraphQLAPI.ps1
@@ -371,6 +371,70 @@ function Connect-GraphQLAPI {
                 return $argString
             }
 
+            function getQueryArgs2 {
+                param ($query)
+                # initialize argument string
+                $argString = ""
+
+                # initialize empty hashtable to store args
+                $queryArgs = @{}
+
+                $arguments = $query.args
+                
+                if ($arguments) {
+                    $argString += "("
+                    foreach ($argument in $arguments) {
+                        
+                        $list = "false"
+                        # Is it required
+                      
+                        if ($argument.type.kind -eq "NON_NULL") {
+                            $required = "!"
+                        } elseif ($argument.type.kind -eq "LIST" ) {
+                            if ($argument.type.ofType.kind -eq "NON_NULL") {
+                                $required = "!"
+                                $list = "true"
+                            }
+                        } else {
+                            $required=""
+                        }
+                        if ($null -eq $argument.type.name ) {
+                            # We need to find the argument type
+                            if ($null -ne $argument.type.ofType.name) {
+                                if ($list -eq "true") {
+                                    $argString += '$' + $argument.name + ': ' + "[$($argument.type.ofType.name)$required] "
+                                }else {
+                                    $argString += '$' + $argument.name + ': ' + "$($argument.type.ofType.name)$required "
+                                }
+                            } else {
+                                # Let's go deeper - this can probably be ported out to a function and recursion eventually
+                                if ($null -ne $argument.type.ofType.ofType.name) {
+                                    if ($list -eq "true") {
+                                        $argSTring += '$' + $argument.name + ': ' + " [$($argument.type.ofType.ofType.name)$required] "
+                                    } else {
+                                        $argSTring += '$' + $argument.name + ': ' + " $($argument.type.ofType.ofType.name)$required "
+                                    }
+                                    
+                                } else {
+                                    $argSTring += '$' + $argument.name + ': ' + " ISSUE FINDING ARG "
+                                }
+                            }
+                        } else {
+                            $argString += '$' + $argument.name + ': ' + $argument.type.name + "$required "
+                        }
+                    }
+                    $argString += " ) "
+                    $argSTring += "{ objects: $($query.name) ("
+                    foreach ($argument in $arguments) {
+                      $argSTring += $argument.name + ': $' + $argument.name + " "
+                    }
+                    $argSTring += ") "
+                } else {
+                    $argSTring += "{ objects: $($query.name) "
+                }
+                return $argString
+            }
+
             # This reference command is used to store the scriptblock of what we want our dynamically-created
             # cmdlets to do
             $ReferenceCommand = {
@@ -493,10 +557,10 @@ function Connect-GraphQLAPI {
                 
                 # Open up QueryString to hold entire query syntax
                 $querystring = "query $($query.name) "
-                #Write-Host "Getting args " -nonewline
-                $arguments = $query.args
+
+                # Include arguments
                 $argString = getQueryArgs $query
-                $querySTring += $argSTring
+                $queryString += $argString
                 #Write-Host "Done" -foregroundcolor "yellow"
 
                 # Get Query Return Type
