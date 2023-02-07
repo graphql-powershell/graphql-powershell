35aaef051ebee79904bcb48f996eb64a0194380e
Mike Preston <mwpreston@gmail.com>
2023-02-07 11:18:38 -0500

    shell for -Properties param

---
+++ private/func_getQueryFromParams.ps1
@@ -0,0 +1,44 @@
+function getQueryFromParams {
+    param (
+        [String[]]$array,
+        [string]$cmdlet
+    )
+
+
+    $array = $array | Sort
+    # Get the longest item
+    $longestItem = 0
+    foreach ($item in $array) {
+        $length = $item.split(".").count - 1
+        if ($longestItem -lt $length) {
+            $longestItem = $length
+        }
+    }
+    
+    $objArray = @()
+    foreach ($item in $array) {
+        $fields = $item.split(".")
+        $obj = New-Object -TypeName PScustomObject
+        $i = 1
+        while ($i -le $longestItem) {
+            $propName = "Item$i"
+            
+            if ($fields[$i]) {
+                $propValue = $fields[$i]
+            } else {
+                $propValue = ""
+            }
+            $obj | Add-Member -NotePropertyName $propName -NotePropertyValue $propValue
+            $i++
+        }
+        $objArray += $obj
+    }
+
+
+    $group = $objArray | Group-Object -Property "Item1"
+
+    $strTest = buildFieldsFromLongNames -group $group -level 1 -maxLevel $longestItem
+
+    return $strTest
+
+}
